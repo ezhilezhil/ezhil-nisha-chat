de06479158ea9d0db9dd5246945861437497cf8f
EZHILARASU V.R <167604422+ezhilezhil@users.noreply.github.com>
2026-07-26 13:43:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -50,6 +50,6 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
